1cd53321ca7f5042e813d6f83db9666b00f00c6d
ChandaB <chandabb@aol.com>
2025-07-15 14:20:04 -0400

    Completed stage02 add getCustomer

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+//import the data access module
+import da from './data-access.js';
+
+//Create an instance of express
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,5 +19,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Start the server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+});
+
+app.get('/customers', async (req, res) => {
+    try {
+        const customers = await da.getCustomers();
+        res.send(customers);
+    } catch (error) {
+        console.error('Error fetching customers:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
